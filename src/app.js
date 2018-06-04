fab6122f2b42321b1d9e59bda8dfbf02a3cb0d38
Your Name <karoliina.mietola@helsinki.fi>
2018-06-04 13:54:36 +0300

    changes

--- src/app.js
+++ src/app.js
@@ -17,13 +17,40 @@ class App extends React.Component {
     console.log(event.target.value)
     this.setState({filter:event.target.value})
   }
+  filterWords =()=> {
+    switch(this.state.translateFrom) {
+      case 'finnish' :
+        return this.state.words.filter(word=>word.finnish.includes(this.state.filter)===true)
+      case 'russian' :
+        return this.state.words.filter(word=>word.russian.includes(this.state.filter)===true)
+      case 'english' :
+        return this.state.words.filter(word=>word.english.includes(this.state.filter)===true)
+      default :
+        return this.state.words
+    }
+  }
+  switchLanguage = () => {
+    console.log('switch')
+    const tmp1=this.state.translateTo
+    const tmp2=this.state.translateFrom
+    this.setState({translateTo: tmp2})
+    this.setState({translateFrom: tmp1})
+  }
+
+
   render() {
+    const shownWords= this.filterWords()
     return (
       <div>
+        <div>
+          <p>finnish
+          <button onClick={this.switchLanguage}> language change </button>
+           russian</p>
+        </div>
         <input value={this.state.filter} onChange={this.handleChange} />
         <table>
           <tbody>
-            {this.state.words.map(word => <tr key={word.id}><td><Word word={word} language={this.state.translateFrom} /></td><td><Word word={word} language={this.state.translateTo} /></td></tr>)}
+            {shownWords.map(word => <tr key={word.id}><td><Word word={word} language={this.state.translateFrom} /></td><td><Word word={word} language={this.state.translateTo} /></td></tr>)}
           </tbody>
         </table>
       </div>
